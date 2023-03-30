ea7eb27432c6802d68fcc8d9c81da5d44e160794
Manith kumar <117072043+Manithkuar03@users.noreply.github.com>
2023-03-30 16:32:10 +0530

    Add files via upload

---
+++ weather/weather.js
@@ -0,0 +1,44 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '735ac00033mshdf69f8bc4ecbfeap191fa7jsn3178edf71ce0',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+const getWeather = (city) => {
+    cityName.innerHTML = city
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            cloud_pct.innerHTML = response.cloud_pct
+            temp.innerHTML = response.temp
+            feels_like.innerHTML = response.feels_like
+            humidity.innerHTML = response.humidity
+            min_temp.innerHTML = response.min_temp
+            max_temp.innerHTML = response.max_temp
+            wind_speed.innerHTML = response.wind_speed
+            wind_degrees.innerHTML = response.wind_degrees
+            sunrise.innerHTML = response.sunrise
+            sunset.innerHTML = response.sunset
+        })
+        .catch(err => console.error(err));
+
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault()
+    getWeather(city.value)
+})
+
+const city = document.getElementById("city");
+getWeather("city")
+
+
+const forecast = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '735ac00033mshdf69f8bc4ecbfeap191fa7jsn3178edf71ce0',
+        'X-RapidAPI-Host': 'https://weather.com/weather/tenday'
+    }
+};
